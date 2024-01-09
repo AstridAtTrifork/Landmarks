97cfe4816f9f942939f20148a9274a65facab439
Astrid Avalin Sørensen <Avalin@users.noreply.github.com>
2024-01-09 09:53:18 +0100

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Astrid Avalin Sørensen on 09/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
