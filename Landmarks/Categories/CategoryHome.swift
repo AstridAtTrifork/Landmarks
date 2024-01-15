2df86e9355b6ed242afa3a79bc9759632fc7361b
Astrid Avalin Sørensen <aav@trifork.com>
2024-01-15 13:26:03 +0100

    CategoryHome now shows enum values

--- Landmarks/Categories/CategoryHome.swift
+++ Landmarks/Categories/CategoryHome.swift
@@ -2,10 +2,16 @@ import SwiftUI
 
 struct CategoryHome: View {
     @Environment(ModelData.self) var modelData
+    
     var body: some View {
         NavigationSplitView {
-            Text("Sidebar")
-                .navigationTitle("Featured")
+            List {
+                ForEach(modelData.categories.keys.sorted(), id: \.self)
+                { key in
+                    Text(key)
+                }
+            }
+            .navigationTitle("Featured")
         } detail: {
             Text("Select a Landmark")
         }
